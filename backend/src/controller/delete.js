74a0b4b162fbe30ef593e688364d4b7f9488c29c
Valdeci97 <moura97.valdeci@gmail.com>
2022-02-21 16:29:54 -0300

    fix: Corrige status code retorno apos deleçao de tarefa

--- backend/src/controller/delete.js
+++ backend/src/controller/delete.js
@@ -3,8 +3,8 @@ const taskModel = require('../model/taskModel');
 const deletetask = async (req, res) => {
   try {
     const { id } = req.params;
-    const result = await taskModel.deleteOne({ '_id': id });
-    return res.status(200).send(result);
+    await taskModel.deleteOne({ '_id': id });
+    return res.status(204).end();
   } catch (err) {
     return res.status(500).send(err);
   }
